e823d3ad232f2724d4577be57caf3f0d3728b9aa
extragalactic <michael.l.dangelo@gmail.com>
2016-01-21 02:57:46 -0500

    cleaned comments added npm scripts

--- app.js
+++ app.js
@@ -15,12 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
 
-/*
-app.use('/', function(req, res, next) {
-  res.sendfile('public/index.html');
-});
-*/
-
 app.get('/', (req, res) => {
   res.render('index');
 });
