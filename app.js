ec22482e52f409fa3e78daf4ad064e87585b3af2
mldangelo <michael.l.dangelo@gmail.com>
2016-04-10 16:32:17 -0700

    added koa convert to remove generator warning

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import Koa from 'koa';
 import views from 'co-views';
 import serve from 'koa-static';
+import convert from 'koa-convert';
 
 const app = new Koa();
 const render = views(__dirname + '/views', { ext: 'jade' });
 
-app.use(serve(__dirname + '/public'));
+app.use(convert(serve(__dirname + '/public')));
 app.use(async (ctx) => {
   const page = await render('index', {});
   ctx.body = page;
