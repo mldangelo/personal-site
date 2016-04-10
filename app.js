8f2f8fae46897aabd6d9e047ca1e3c0e10a00e45
mldangelo <michael.l.dangelo@gmail.com>
2016-04-10 01:34:57 -0700

    initial config with koa

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+var views = require('co-views');
+var koa = require('koa');
+const app = module.exports = new koa();
 
-const app = express();
+// setup views, appending .ejs
+// when no extname is given to render()
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'jade');
+var render = views(__dirname + '/views', { ext: 'jade' });
 
-app.get('/', (req, res) => {
-  res.render('index');
+// render
+
+app.use(function *(){
+  this.body = yield render('index', {});
 });
 
-app.get('*', (req, res) => {
-  res.status(302).redirect('/');
-});
-
-app.use((err, req, res, next) => {
-  res.redirect('/');
-});
-
-
-module.exports = app;
+if (!module.parent) app.listen(7999);
