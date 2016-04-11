56c529926e0015efebce16e89c57e8c755d66ab7
mldangelo <michael.l.dangelo@gmail.com>
2016-04-10 17:12:59 -0700

    added basic react app converted jade template to pre-rendered html added webpack configuration updated gitignore

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App.js';
+
+ReactDOM.render(
+    <div>
+      <h1>Hello World</h1>
+    </div>,
+    document.getElementById('root')
+);
