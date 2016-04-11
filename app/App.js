54c2e36bd9fd1578346ba4d4852c1adeb4c00981
mldangelo <michael.l.dangelo@gmail.com>
2016-04-10 20:17:17 -0700

    starting to build more full featured react app

--- app/App.js
+++ app/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1> Hello World </h1>
+        <h2> hi </h2>
+        <Nav/>
       </div>
     );
   }
---
+++ app/components/Nav.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
