9eda82a3be0f2bcaf31217bbe12a266757f91844
mldangelo <michael.l.dangelo@gmail.com>
2016-04-14 19:52:53 -0700

    splitting style sheet into sheets

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-const css = require("!style!css!sass!../public/css/main.scss");
+const css = require("!style!css!sass!../public/css/site.scss");
 
 ReactDOM.render(
     <div>
