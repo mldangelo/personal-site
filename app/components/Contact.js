43974bd249d41eb95edd4bb630c1001812268552
mldangelo <michael.l.dangelo@gmail.com>
2016-04-22 07:31:16 -0700

    contact methods now populated from react -- why?

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,6 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component, PropTypes } from 'react';
+
+const data = [
+  {
+    label: "Email",
+    value: "mailto:michael.l.dangelo@gmail.com"
+  },{
+    label: "Facebook",
+    value: "https://facebook.com/d"
+  },{
+    label: "LinkedIn",
+    value: "https://www.linkedin.com/in/michaelldangelo"
+  },{
+    label: "Instagram",
+    value: "https://www.instagram.com/dangelosaurus"
+  },{
+    label: "Angel List",
+    value: "https://angel.co/michael-d-angelo"
+  }
+];
+
+class Row extends Component {
+  render() {
+    return (
+      <span>
+        <a href={this.props.pair.value}>{this.props.pair.label}</a>
+        <br/>
+      </span>
+    );
+  }
+}
+
+Row.propTypes = {
+  pair: PropTypes.object.isRequired,
+};
+
 
 class Contact extends Component {
+
+  getRows(){
+    return data.map((pair) => {
+      return <Row pair={pair} />
+    });
+  }
   render() {
     return (
       <div className="row no-margin-lr">
@@ -10,15 +51,7 @@ class Contact extends Component {
                     <h3>Contact Me</h3>
                 </header>
                 <p>
-                  <a href="mailto:michael.l.dangelo@gmail.com">Email</a>
-                  <br/>
-                  <a href="https://facebook.com/d">Facebook</a>
-                  <br/>
-                  <a href="https://www.linkedin.com/in/michaelldangelo">LinkedIn</a>
-                  <br/>
-                  <a href="https://www.instagram.com/dangelosaurus">Instagram</a>
-                  <br/>
-                  <a href="https://angel.co/michael-d-angelo">Angel List</a>
+                  {this.getRows()}
                 </p>
             </div>
         </div>
