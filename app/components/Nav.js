bedd3c99cdd834ea5b7b42234c6d62cef9a9ea5f
mldangelo <michael.l.dangelo@gmail.com>
2016-06-17 23:08:58 -0400

    rebuild nav menu

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,15 +3,37 @@ import React, {Component} from 'react';
 class Nav extends Component {
   render() {
     return (
-      <div className="container side-nav">
-        <h1>MICHAEL D'ANGELO</h1>
-        <h2><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></h2>
-        <br/>
-        <img src="/images/me.jpg" alt="" className="img-responsive"/>
-        <br/>
-        <h3>About Me</h3>
-        <p>Hi, I'm Michael. I like building things. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate and the CTO of <a href="http://arthena.com">Arthena</a>. Before Arthena I was at <a href="http://planet.com">Planet Labs</a>, <a href="http://planetaryresources.com">Planetary Resources</a>, <a href="http://facebook.com">Facebook</a>, and <a href="http://seds.org">SEDS</a>.</p>
-      </div>
+      <section id="sidebar">
+        {/* <!-- Intro --> */}
+          <section id="intro">
+            <a href="#" className="logo"><img src="http://localhost:7999/images/me.jpg" alt="" /></a>
+            <header>
+              <h2>Michael D'Angelo</h2>
+              <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+            </header>
+          </section>
+
+        {/* <!-- About --> */}
+          <section className="blurb">
+            <h2>About</h2>
+              <p>Hi, I'm Michael. I like building things. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate and the CTO of <a href="http://arthena.com">Arthena</a>. Before Arthena I was at <a href="http://planet.com">Planet Labs</a>, <a href="http://planetaryresources.com">Planetary Resources</a>, <a href="http://facebook.com">Facebook</a>, and <a href="http://seds.org">SEDS</a>.</p>
+            <ul className="actions">
+              <li><a href="#" className="button">Learn More</a></li>
+            </ul>
+          </section>
+
+        {/*  <!-- Footer -->  */}
+          <section id="footer">
+            <ul className="icons">
+              <li><a href="#" className="fa-twitter"><span className="label">Twitter</span></a></li>
+              <li><a href="#" className="fa-facebook"><span className="label">Facebook</span></a></li>
+              <li><a href="#" className="fa-instagram"><span className="label">Instagram</span></a></li>
+              <li><a href="#" className="fa-rss"><span className="label">RSS</span></a></li>
+              <li><a href="#" className="fa-envelope"><span className="label">Email</span></a></li>
+            </ul>
+            <p className="copyright">&copy; Michael D'Angelo <a href="http://mldangelo.com">mldangelo.com</a>.</p>
+          </section>
+      </section>
     );
   }
 }
