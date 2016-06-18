a7f054ad22f9ae5fc7314717b843a6050a18f6cc
mldangelo <michael.l.dangelo@gmail.com>
2016-06-18 03:13:08 -0400

    redid education section of resume

--- app/components/Resume/Education.js
+++ app/components/Resume/Education.js
@@ -1,6 +1,47 @@
 import React, {Component, PropTypes} from 'react';
 
+const degrees = [
+  {
+    school: 'Stanford University',
+    degree: 'M.S. Computational and Mathematical Engineering',
+    link: 'https://stanford.edu',
+    year: '2016',
+  }, {
+    school: 'University at Buffalo',
+    degree: 'B.S. Electical Engineering, Computer Engineering',
+    link: 'https://buffalo.edu',
+    year: '2012',
+  },
+];
+
+class Degree extends Component {
+
+  render() {
+    return (
+      <article>
+        <header>
+          <p className="degree">{this.props.data.degree}</p>
+          <p className="school"><a href={this.props.data.link}>{this.props.data.school}</a>, {this.props.data.year}</p>
+        </header>
+      </article>
+    );
+  }
+}
+
+Degree.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+
 class Education extends Component {
+
+  getRows() {
+    return degrees.map((degree) => {
+      return <Degree
+        data={degree}
+        key={degree.school} />;
+    });
+  }
 
   render() {
     return (
@@ -8,14 +49,7 @@ class Education extends Component {
         <div className="title">
           <h3>Education</h3>
         </div>
-        <div className="school">
-          <p>M.S. Computational and Mathematical Engineering</p>
-          <p>Stanford University. 2016.</p>
-        </div>
-        <div className="school">
-          <p>B.S. Electical Engineering, Computer Engineering</p>
-          <p>University at Buffalo. 2012. Graduated first in class.</p>
-        </div>
+        {this.getRows()}
       </div>
     );
   }
