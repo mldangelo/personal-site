a766c222766ef5648e999b2a6ffa9f2e8520609a
mldangelo <michael.l.dangelo@gmail.com>
2016-06-19 19:43:24 -0400

    passing github data through api route

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,48 @@
+import _ from 'lodash';
 import GitHubApi from 'github';
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 const github = new GitHubApi();
 
-github.authenticate({
-    type: "oauth",
-    token: process.env.GITHUB_OAUTH,
-});
+const authenticate = (cb) => {
+  github.authenticate({
+      type: "oauth",
+      token: process.env.GITHUB_OAUTH,
+  });
+  if (typeof callback === 'function') cb();
+};
+
+authenticate();
 
 const routes = (app) => {
-
+  // TODO Cache to DB later
   app.get('/api/github', (req, res) => {
-    github.users.get({}, function(err, _res) {
-        console.log(err, res);
-        res.send(_res);
+    github.repos.get({
+      user: 'typpo',
+      repo: 'ad-detector',
+    }, function(err, _res) {
+      if(err) console.log(err);
+        const data = _.pick(_res,
+          ['stargazers_count',
+          'watchers_count',
+          'forks',
+          'open_issues_count',
+          'subscribers_count',
+          'pushed_at',
+        ]);
+
+        if (err && err.status == 'Unauthorized'){
+          authenticate(() => { // retry authentication -- if token expires
+            res.send(data);
+          });
+        } else {
+          res.send(data);
+        }
+
     });
   });
+
 
 };
 
