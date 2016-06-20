3c1fd69f5ce069f12af5f53be094b91cd0e23768
mldangelo <michael.l.dangelo@gmail.com>
2016-06-20 07:29:51 -0400

    split hamburger into separate file

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,58 +1,7 @@
 import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
 
-import Menus from 'react-burger-menu';
-const Menu = Menus.slide;
-
-class Hamburger extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      open:  false,
-    };
-  }
-
-  handleClick() {
-    console.log('open:',this.state.open);
-    this.setState({
-      open: !this.state.open,
-    });
-  }
-
-  getButton() {
-    return this.state.open ? (
-      <li className="menu close-menu" onClick={this.handleClick.bind(this)}>
-        <a className="fa-times close-menu">Menu</a>
-      </li>
-    ) : (
-      <li className="menu open-menu" onClick={this.handleClick.bind(this)}>
-        <a className="fa-bars">Menu</a>
-      </li>
-    );
-  }
-
-  render() {
-    return (
-        <div className="hamburger-container">
-          <nav className="main" id="hambuger-nav">
-            <ul>
-              {this.getButton()}
-            </ul>
-          </nav>
-          <Menu right noOverlay isOpen={this.state.open}>
-            <IndexLink to="/">MICHAEL D'ANGELO</IndexLink>
-            <a></a><Link className="hamburger-link" to="/resume">RESUME</Link>
-            <a></a><Link to="/projects">PROJECTS</Link>
-            <a></a><Link to="/stats">STATS</Link>
-            <a></a><Link to="/contact">CONTACT</Link>
-          </Menu>
-        </div>
-    );
-  }
-
-}
-
+import Hamburger from './Header/Hamburger';
 
 class Header extends Component {
 
