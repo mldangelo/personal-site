f0985766067545a1f4eeebf027ecdf4215913f3f
mldangelo <michael.l.dangelo@gmail.com>
2016-06-19 21:25:42 -0400

    updated github route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import GitHubApi from 'github';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -13,52 +14,45 @@ const authenticate = (cb) => {
   if (typeof callback === 'function') cb();
 };
 
-authenticate();
+let authenticated = false;
 
 const routes = (app) => {
 
-
-  app.get('/api/github', (req, res) => {
-    const data = {
-      forks: 21,
-      open_issues_count: 10,
-      pushed_at: "2016-03-25T17:01:39Z",
-      stargazers_count: 171,
-      subscribers_count: 18,
-      watchers_count:171,
-    };
-    res.send(JSON.stringify(data));
-  })
-
   // TODO Cache to DB later
-  app.get('/api/github2', (req, res) => {
+  app.get('/api/github', (req, res) => {
+
+    // Handles authentication for the first time
+    if (!authenticated) {
+      authenticated = true;
+      authenticate();
+    }
+
     github.repos.get({
-      user: 'typpo',
+      user: 'typpo', // TODO Change to this repo
       repo: 'ad-detector',
-    }, function(err, _res) {
+    }, (err, _res) => {
       if(err) console.log(err);
-        const data = _.pick(_res,
+      const send = () => {
+        res.send(JSON.stringify(
+          _.pick(_res,
           ['stargazers_count',
           'watchers_count',
           'forks',
           'open_issues_count',
           'subscribers_count',
           'pushed_at',
-        ]);
+        ])));
+      };
 
-        const send = () => {
-          res.send(JSON.stringify(data));
-        };
-
-        if (err && err.status == 'Unauthorized'){
-          authenticate(send()); // retry authentication -- if token expires
-        } else {
-          send();
-        }
-
+      if (err && err.status == 'Unauthorized'){
+        authenticate(send()); // retry authentication -- if token expires
+      } else if (err) {
+        res.send(JSON.stringify({})); // keep default values
+      } else {
+        send();
+      }
     });
   });
-
 
 };
 
