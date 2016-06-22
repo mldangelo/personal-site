f30b7dee37f8c016e0424c2b54f22c2e5862b36c
mldangelo <michael.l.dangelo@gmail.com>
2016-06-22 19:18:47 -0400

    split courses into one per line for small viewports

--- app/components/Resume/Courses/Course.js
+++ app/components/Resume/Courses/Course.js
@@ -6,12 +6,12 @@ class Course extends Component {
 
   render() {
     return (
-      <li>
+      <li className="course-container">
         <a href={this.props.data.link}>
           <h4 className="course-number">{this.props.data.number}:</h4>
           <p className="course-name">{this.props.data.title}</p>
         </a>
-        {this.props.last ? null : <p className="course-name"> &#8226;</p>}
+        {this.props.last ? null : <div className="course-dot"><p className="course-name"> &#8226;</p></div>}
       </li>
     );
   }
