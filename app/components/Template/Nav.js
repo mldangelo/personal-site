d468f5ad7687e3ead3d4ca696072dfa42faea274
mldangelo <michael.l.dangelo@gmail.com>
2016-06-22 17:25:02 -0400

    changed planet labs to planet

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -18,7 +18,7 @@ class Nav extends Component {
               <p>Hi, I'm Michael. I like building things.
                 I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate and
                 the CTO of <a href="http://arthena.com">Arthena</a>. Before Arthena I was
-                at <a href="http://planet.com">Planet<s> Labs</s></a>, <a href="http://planetaryresources.com">Planetary Resources</a>, <a href="http://facebook.com">Facebook</a>, and <a href="http://seds.org">SEDS</a>.</p>
+                at <a href="http://planet.com">Planet</a>, <a href="http://planetaryresources.com">Planetary Resources</a>, <a href="http://facebook.com">Facebook</a>, and <a href="http://seds.org">SEDS</a>.</p>
             <ul className="actions">
               <li><Link to="/resume" className="button">Learn More</Link></li>
             </ul>
