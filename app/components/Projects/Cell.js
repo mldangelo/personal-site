b762b672711ec24d594ffccc6fdb230de3ca1dcc
mldangelo <michael.l.dangelo@gmail.com>
2017-02-05 03:04:34 -0800

    extremely ugly css for project descriptions

--- app/components/Projects/Cell.js
+++ app/components/Projects/Cell.js
@@ -2,13 +2,18 @@ import React, { PropTypes } from 'react';
 import moment from 'moment';
 
 const Cell = props => (
+  <div className="cell-container">
   <article className="mini-post">
     <header>
       <h3><a href={props.data.link}>{props.data.title}</a></h3>
       <time className="published">{moment(props.data.date).format('MMMM, YYYY')}</time>
     </header>
     <a href={props.data.link} className="image"><img src={props.data.image} alt="" /></a>
+    <div className="description">
+      <p>{props.data.desc}</p>
+    </div> 
   </article>
+  </div>
 );
 
 Cell.propTypes = {
