867b15f55a345365c8ae245ce70ca08b26cdba83
mldangelo <michael.l.dangelo@gmail.com>
2017-02-05 01:42:32 -0800

    removed redundant date field

--- app/components/Projects/Cell.js
+++ app/components/Projects/Cell.js
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react';
+import moment from 'moment';
 
 const Cell = props => (
   <article className="mini-post">
     <header>
       <h3><a href={props.data.link}>{props.data.title}</a></h3>
-      <time className="published" dateTime={props.data.datetime}>{props.data.date}</time>
+      <time className="published">{moment(props.data.date).format('MMMM, YYYY')}</time>
     </header>
     <a href={props.data.link} className="image"><img src={props.data.image} alt="" /></a>
   </article>
@@ -16,7 +17,7 @@ Cell.propTypes = {
     link: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
-    datetime: PropTypes.string.isRequired,
+    // datetime: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- app/components/Projects/data/projects.js
+++ app/components/Projects/data/projects.js
@@ -4,26 +4,22 @@ const data = [
     title: 'Nearest Dollar',
     link: 'http://www.nearestdollar.com/',
     image: '/images/projects/nearestdollar.jpg',
-    date: 'November, 2015', // TODO - Derive this from datetime field
-    datetime: '11-20-2015',
+    date: '2015-11-20',
   }, {
     title: 'Harvest',
     link: 'http://www.harvesters.club/',
     image: '/images/projects/harvest.jpg',
-    date: 'September, 2015',
-    datetime: '09-20-2015',
+    date: '2015-09-20',
   }, {
     title: 'Space Potato',
     link: 'http://www.spacepotato.org',
     image: '/images/projects/spacepotato.jpg',
-    date: 'June, 2015',
-    datetime: '06-28-2015',
+    date: '2015-06-28',
   }, {
     title: 'Cat Detector',
     link: 'http://www.catdetector.biz',
     image: '/images/projects/catdetector.jpg',
-    date: 'May, 2015',
-    datetime: '05-15-2015',
+    date: '2015-05-15',
   },
 ];
 
