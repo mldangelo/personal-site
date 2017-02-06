6d2d9daf2fccb4df22f47a51dee278c67a6f9860
mldangelo <michael.l.dangelo@gmail.com>
2017-02-05 19:04:42 -0800

    updated github api to latest version moved data folders around refactored api call

--- app/data/github.js
+++ app/data/github.js
@@ -1,5 +1,15 @@
 import moment from 'moment';
 
+// Keys returned by the github api
+export const githubKeys = [
+  'stargazers_count',
+  'watchers_count',
+  'forks',
+  'open_issues_count',
+  'subscribers_count',
+  'pushed_at',
+];
+
 // TODO To be provided by an API
 const data = [
   {
