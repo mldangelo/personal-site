a1beef7893b06d0397966a3d84f1f17ebd4f427d
mldangelo <michael.l.dangelo@gmail.com>
2017-02-05 18:23:05 -0800

    refactored contact links for codeclimate

---
+++ app/data/contact.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    link: "https://github.com/mldangelo",
+    label: 'Github',
+    icon: "fa-github",
+  },
+  {
+    link: "https://facebook.com/d",
+    label: 'Facebook',
+    icon: "fa-facebook",
+  },
+  {
+    link: "https://www.instagram.com/dangelosaurus/",
+    label: 'Instagram',
+    icon: "fa-instagram",
+  },
+  {
+    link: "https://www.linkedin.com/in/michaelldangelo",
+    label: 'LinkedIn',
+    icon: "fa-linkedin",
+  },
+  {
+    link: "https://angel.co/michael-d-angelo",
+    label: 'Angel List',
+    icon: "fa-angellist",
+  },
+  {
+    link: "https://twitter.com/dangelosaurus",
+    label: 'Twitter',
+    icon: "fa-twitter",
+  },
+  {
+    link: "mailto:michael.l.dangelo@gmail.com",
+    label: 'Email',
+    icon: "fa-envelope",
+  },
+];
+
+export default data;
