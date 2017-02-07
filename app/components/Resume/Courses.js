87aa6dc263b401960ff34744f612f1f7c9147a60
mldangelo <michael.l.dangelo@gmail.com>
2017-02-06 22:40:35 -0800

    removed other orderby no longer wrap course numbers

--- app/components/Resume/Courses.js
+++ app/components/Resume/Courses.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
-import _orderBy from 'lodash/orderBy';
-
 import Course from './Courses/Course';
 import courses from '../../data/courses';
 
 const getRows = () => {
-  const ordered = _orderBy(courses, ['univerity', 'number'], ['desc', 'asc']);
+  const ordered = courses.sort((a, b) => {
+    let ret = 0;
+    if (a.university > b.university) ret = -1;
+    else if (a.unversity < b.university) ret = 1;
+    else if (a.number > b.number) ret = 1;
+    else if (a.number < b.number) ret = -1;
+    return ret;
+  })
+  
   const rows = ordered.map(course => (
     <Course
       data={course}
       key={course.title}
-    />));
+    />
+  ));
+    
   // Remove dot after last course
   const lastidx = ordered.length - 1;
   rows[lastidx] = (
@@ -30,7 +38,7 @@ const Courses = () => (
       <div className="title">
         <h3>Selected Courses</h3>
       </div>
-      <ul>
+      <ul className="course-list">
         {getRows()}
       </ul>
     </div>
