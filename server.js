ce984e7ccba61d2f321afef8524761c76981f25a
mldangelo <michael.l.dangelo@gmail.com>
2017-02-13 21:37:54 -0800

    removed old todo

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 apiRoutes(app);
 
-// TODO(Michael) Rewrite linter rule for string compare
 if (env === 'development') { // eslint-disable-line eqeqeq
   const compiler = webpack(config);
   const middleware = webpackMiddleware(compiler, {
@@ -63,7 +62,7 @@ app.listen(port, '0.0.0.0', (err) => {
   if (err) {
     console.error('application-err', err);
   }
-  console.info(`Started in ${env === 'development' ? 'development' : 'production'} mode on port ${port}.`);
+  console.info(`Started in ${env === 'development' ? env : 'production'} mode on port ${port}.`);
 });
 
 export default app;
