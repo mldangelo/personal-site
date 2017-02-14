70421de54b727389571192ffbcf54e4ea0c39d42
mldangelo <michael.l.dangelo@gmail.com>
2017-02-14 00:08:14 -0800

    build webpack to /tmp, then move to /dist for production builds

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path.join(__dirname, '../app/main.js'),
   ],
   output: {
-    path: path.join(__dirname, '../dist/'),
+    path: path.join(__dirname, '../tmp/'),
     filename: '[name]-[hash].min.js',
     publicPath: '/dist/',
   },
