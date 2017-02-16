3e31b1724942ed0c607217936fb7dde2f0669904
mldangelo <michael.l.dangelo@gmail.com>
2017-02-16 01:54:10 -0800

    added last fm api to scrape top artists

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,20 @@
-import GitHubApi from 'github';
+import 'dotenv/config';
 import moment from 'moment';
-import dotenv from 'dotenv';
+
+import GithubAPI from 'github';
+import LastfmAPI from 'last.fm.api';
 
 import { githubKeys as keys } from '../app/data/github';
 
-dotenv.config();
+const cached = {};
 
-let cached = {};
+const lastfm = new LastfmAPI({
+  apiKey: process.env.LASTFM_KEY,
+});
 
-const github = new GitHubApi();
+const github = new GithubAPI();
 
-const authenticate = (cb) => {
+const authenticateGH = (cb) => {
   github.authenticate({
     type: 'oauth',
     token: process.env.GITHUB_OAUTH,
@@ -18,37 +22,66 @@ const authenticate = (cb) => {
   if (typeof cb === 'function') cb();
 };
 
-const send = (res, _res) => {
-  const data = keys.reduce((obj, key) => ({
-    ...obj,
-    [key]: _res[key],
-  }), {});
-  data.pushed_at = moment(data.pushed_at).format('MMMM DD, YYYY');
-  data.updated_at = Date.now();
-  cached = data;
-  res.send(JSON.stringify(data));
-};
-
-authenticate();
+authenticateGH();
 
 const routes = (app) => {
   app.get('/api/github', (req, res) => {
-    if (cached.updated_at && (Date.now() - cached.updated_at) / 1000 < 60) {
-      res.send(JSON.stringify(cached));
+    const send = (payload) => {
+      const data = keys.reduce((obj, key) => ({
+        ...obj,
+        [key]: payload[key],
+      }), {});
+      data.pushed_at = moment(data.pushed_at).format('MMMM DD, YYYY');
+      data.updated_at = Date.now();
+      cached.github = data;
+      res.send(JSON.stringify(data));
+    };
+
+    if (cached.github && cached.github.updated_at &&
+      ((Date.now() - cached.github.updated_at) / 1000 < 60)) {
+      res.send(JSON.stringify(cached.github));
     } else {
       github.repos.get({
         owner: 'mldangelo',
         repo: 'mldangelo',
-      }, (err, _res) => {
+      }, (err, payload) => {
         if (err && err.status === 'Unauthorized') {
           console.error('github-api-unauthorized-error', err);
-          authenticate(send(res, _res)); // retry authentication -- if token expires
+          authenticateGH(send(payload)); // retry authentication -- if token expires
         } else if (err) {
           console.error('github-api-error', err);
-          res.send(JSON.stringify(cached)); // keep cached values
+          res.send(JSON.stringify(cached.github)); // keep cached values
         } else {
-          send(res, _res);
+          send(payload);
         }
+      });
+    }
+  });
+
+  app.get('/api/lastfm', (req, res) => {
+    if (cached.lastfm && cached.lastfm.updated_at &&
+      ((Date.now() - cached.lastfm.updated_at) / 1000 < 60)) {
+      res.send(JSON.stringify(cached.lastfm.artists));
+    } else {
+      lastfm.user.getTopArtists({
+        user: process.env.LASTFM_USERNAME,
+        period: '12month',
+      })
+      .then((payload) => {
+        const data = {
+          artists: payload.topartists.artist.map(artist => ({
+            name: artist.name,
+            count: artist.playcount,
+            link: artist.url,
+          })),
+          updated_at: Date.now(),
+        };
+        cached.lastfm = data;
+        res.send(JSON.stringify(data.artists));
+      })
+      .catch((err) => {
+        console.error('lastfm-api-error', err);
+        res.send(cached.lastfm.artists);
       });
     }
   });
