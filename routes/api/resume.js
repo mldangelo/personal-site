0a4f75d0d167db23bbfd83e9feb6440cd9ce234d
mldangelo <michael.l.dangelo@gmail.com>
2017-03-25 00:55:28 -0700

    moved private data into protected folder

--- routes/api/resume.js
+++ routes/api/resume.js
@@ -1,7 +1,7 @@
-import courses from '../../app/data/courses';
-import degrees from '../../app/data/degrees';
-import positions from '../../app/data/positions';
-import { skills, categories } from '../../app/data/skills';
+import courses from '../../app/data/protected/courses';
+import degrees from '../../app/data/protected/degrees';
+import positions from '../../app/data/protected/positions';
+import { skills, categories } from '../../app/data/protected/skills';
 
 export default (req, res) => {
   res.json({
