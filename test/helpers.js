abbe91f70a79aa51fbae5dba78cf4fb3598e7dcd
mldangelo <michael.l.dangelo@gmail.com>
2017-03-28 14:27:17 -0700

    removed login test

--- test/helpers.js
+++ test/helpers.js
@@ -35,11 +35,6 @@ const pages = [
     title: 'Music | Michael D\'Angelo',
     heading: 'SOME BANDS THAT I LIKE',
   },
-  {
-    route: '/login',
-    title: 'Login | Michael D\'Angelo',
-    heading: 'LOGIN',
-  },
 ];
 
 export { pages, randomString };
