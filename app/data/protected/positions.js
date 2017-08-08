7961ac83730097887108355508e0aaeb21075369
mldangelo <michael.l.dangelo@gmail.com>
2017-08-08 16:24:01 -0400

    updated dependencies

--- app/data/protected/positions.js
+++ app/data/protected/positions.js
@@ -17,7 +17,7 @@ const positions = [
     link: 'http://enveritas.org',
     daterange: 'September 2016 - Present',
     points: [
-      'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accurracy.',
+      'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
       'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
       'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM’s, and other techniques.',
     ],
