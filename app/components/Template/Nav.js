66de9c411019e008d3ce13959d45073f11a4fd09
mldangelo <michael.l.dangelo@gmail.com>
2018-09-23 15:37:02 -0400

    upgraded dependencies, fixed warnings

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import data from '../../data/contact';
 
