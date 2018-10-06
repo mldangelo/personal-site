61b38cfc7e958482c9064c44d982e642b613a4fc
mldangelo <michael.l.dangelo@gmail.com>
2018-10-06 00:40:04 -0400

    remove scripts, copy cname

--- scripts/kill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script to kill running instances of site on port 7999
-
-	pid=$(sudo netstat -tulpn | grep 7999 | grep node | tr '/' ' ' | awk '{print $7}')
-	$(sudo kill -9 $pid)
-	echo killed $pid
