7d8deae45df11e2c0970eb0c11490dbe31590dc1
mldangelo <michael.l.dangelo@gmail.com>
2019-02-09 14:36:02 -0500

    refactored apis

--- server/routes/api/github.js
+++ server/routes/api/github.js
@@ -11,33 +11,27 @@ const github = new GithubAPI({
   auth: `token ${process.env.GITHUB_OAUTH}`,
 });
 
-const apiCall = async (req, res) => {
-  const send = (payload) => {
-    const data = keys.reduce((obj, key) => ({
-      ...obj,
-      [key]: payload[key],
-    }), {});
-    data.pushed_at = dayjs(data.pushed_at).format('MMMM DD, YYYY');
-    data.updated_at = Date.now();
-    cached = data;
-    res.send(JSON.stringify(data));
-  };
+const updateCache = (payload) => {
+  const data = keys.reduce((obj, key) => ({
+    ...obj,
+    [key]: payload[key],
+  }), {});
+  data.pushed_at = dayjs(data.pushed_at).format('MMMM DD, YYYY');
+  data.updated_at = Date.now();
+  cached = data;
+};
 
-  if (cached.updated_at
-    && ((Date.now() - cached.updated_at) / 1000 < 60)) {
-    res.send(JSON.stringify(cached));
-  } else {
-    const { data } = await github.repos.get({
+const apiCall = async (req, res) => {
+  if (!cached.updated_at || ((Date.now() - cached.updated_at) / 1000 > 60)) {
+    const { status, data } = await github.repos.get({
       owner: 'mldangelo',
       repo: 'personal-site',
     });
-    if (!data) {
-      console.error('github-api-error');
-      res.send(JSON.stringify(cached)); // keep cached values
-    } else {
-      send(data);
+    if (status === 200) {
+      updateCache(data);
     }
   }
+  res.send(JSON.stringify(cached));
 };
 
 export default apiCall;
