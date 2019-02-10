ca98c61a4afaea5495439c5ae41545e2915360a7
mldangelo <michael.l.dangelo@gmail.com>
2019-02-10 00:38:09 -0500

    pass linter

--- app/entry.js
+++ app/entry.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router, Redirect, Switch, Route,
+  BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 
 // Featured
