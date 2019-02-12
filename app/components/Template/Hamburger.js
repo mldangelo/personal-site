2ab08aa3729ec3d205cb7f8d04500817692ca95f
mldangelo <michael.l.dangelo@gmail.com>
2019-02-12 02:08:19 -0500

    remove unused dependency from bundle

--- app/components/Template/Hamburger.js
+++ app/components/Template/Hamburger.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import Menus from 'react-burger-menu';
-import cookie from 'js-cookie';
+import cookie from 'js-cookie'
+import Menu from 'react-burger-menu/lib/menus/slide';
 
 import routes from '../../data/routes';
-
-const { slide: Menu } = Menus;
 
 const Hamburger = () => {
   const [open, setOpen] = useState(false);
