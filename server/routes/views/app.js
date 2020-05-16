63c32249151b83e65460952fcb3843fcf0b37a07
mldangelo <michael.l.dangelo@gmail.com>
2020-05-16 06:53:51 -0400

    linted

--- server/routes/views/app.js
+++ server/routes/views/app.js
@@ -8,7 +8,7 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '../../../webpack/webpack.config.babel.js';
+import config from '../../../webpack/webpack.config.babel';
 
 const env = process.env.NODE_ENV || 'development';
 
