2e4585ea09218e98eb07604967490a43b28f3cb1
mldangelo <michael.l.dangelo@gmail.com>
2020-12-20 02:41:43 -0500

    convert stats page

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-
-import Main from '../layouts/Main';
+import Link from 'next/link';
+import { NextSeo } from 'next-seo';
 
 import Personal from '../components/Stats/Personal';
 import Site from '../components/Stats/Site';
 
 const Stats = () => (
-  <Main>
-    <Helmet title="Stats" />
+  <>
+    <NextSeo
+      title="Stats | Michael D'Angelo"
+      description="Learn interesting statistics about Michael D'Angelo."
+    />
     <article className="post" id="stats">
       <header>
         <div className="title">
-          <h2><Link to="/stats">Stats</Link></h2>
+          <h2><Link href="/stats">Stats</Link></h2>
         </div>
       </header>
       <Personal />
       <Site />
     </article>
-  </Main>
+  </>
 );
 
 export default Stats;
