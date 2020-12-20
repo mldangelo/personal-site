d14bbfa039760e5d36f4531652da5ccbeb8d0c5c
mldangelo <michael.l.dangelo@gmail.com>
2020-12-20 03:14:14 -0500

    eliminate unnecessary files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { NextSeo } from 'next-seo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
