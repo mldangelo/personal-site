d984a42ea319d7ecbd00bfd325b67afd5212318a
mldangelo <michael.l.dangelo@gmail.com>
2020-12-20 02:19:11 -0500

    start to mock out default layout

---
+++ src/pages/_document.js
@@ -0,0 +1,41 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Footer from '../components/footer';
+// Adapted from https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+
+const { GA_ID } = process.env;
+
+/* eslint-disable react/no-danger */
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {GA_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
+              gtag('config', '${GA_ID}', { page_path: window.location.pathname });`,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <Footer />
+        </body>
+      </Html>
+    );
+  }
+}
+/* eslint-enable react/no-danger */
+
+export default MyDocument;
