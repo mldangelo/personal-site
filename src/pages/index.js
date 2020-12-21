1b022cc26c0de38a76362a4f0e5f41050391655b
mldangelo <michael.l.dangelo@gmail.com>
2020-12-20 20:45:35 -0500

    improve index text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import Link from 'next/link';
+import { NextSeo } from 'next-seo';
 
 const Index = () => (
   <>
+    <NextSeo
+      title="Michael D'Angelo"
+      description="Welcome to Michael D'Angelo's personal website."
+    />
     <article className="post" id="index">
       <header>
         <div className="title">
@@ -9,12 +14,14 @@ const Index = () => (
             <Link href="/">About this site</Link>
           </h2>
           <p>
-            A beautiful, responsive, react app written with modern Javascript.
+            A beautiful, responsive,{' '}
+            <a href="https://jamstack.org/">JAMstack</a> app written with{' '}
+            <a href="https://nextjs.org/">Next.js</a>,{' '}
+            <a href="https://reactjs.org/">React</a>, and modern Javascript.
           </p>
         </div>
       </header>
       <p>
-        {' '}
         Welcome to my website. Please feel free to read more{' '}
         <Link href="/about">about me</Link>, or you can check out my{' '}
         <Link href="/resume">resume</Link>,{' '}
@@ -24,8 +31,11 @@ const Index = () => (
       </p>
       <p>
         {' '}
-        Source available{' '}
-        <a href="https://github.com/mldangelo/personal-site">here</a>.
+        Source available at{' '}
+        <a href="https://github.com/mldangelo/personal-site">
+          @mldangelo/personal-site
+        </a>
+        .
       </p>
     </article>
   </>
