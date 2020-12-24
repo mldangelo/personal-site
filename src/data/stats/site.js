dcf036edc8aaa8fb647af14c74887a138b37bc73
mldangelo <michael.l.dangelo@gmail.com>
2020-12-24 13:16:22 -0500

    update loc

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -44,7 +44,7 @@ const data = [
     /* find . | grep ".js" | grep -vE ".min.js|node_modules|.git|.json" |
     xargs -I file cat file | wc -l */
     label: 'Lines of Javascript powering this website',
-    value: '1806',
+    value: '2115',
     link: 'https://github.com/mldangelo/personal-site/graphs/contributors',
   },
 ];
