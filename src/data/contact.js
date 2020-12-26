ee1e7a8c021aa8553cc0e5f7ace36988ce6b5aab
mldangelo <michael.l.dangelo@gmail.com>
2020-12-25 23:19:09 -0500

    update comment

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,6 +5,8 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+// See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
+// to add other icons.
 
 const data = [
   {
