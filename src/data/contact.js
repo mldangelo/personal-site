f80ea607d26ad3b54360428bebcb3c472555befc
mldangelo <michael.l.dangelo@gmail.com>
2020-12-25 23:18:06 -0500

    use facebook f

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,5 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
@@ -15,7 +15,7 @@ const data = [
   {
     link: 'https://facebook.com/d',
     label: 'Facebook',
-    icon: faFacebook,
+    icon: faFacebookF,
   },
   {
     link: 'https://www.instagram.com/dangelosaurus/',
