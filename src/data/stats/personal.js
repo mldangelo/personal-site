cd4acb145bae20ac5a6a0679770a8c6688d256a6
mldangelo <michael.l.dangelo@gmail.com>
2021-01-02 21:33:24 -0500

    make age calculation more accurate

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 
 const Age = () => {
   const [age, setAge] = useState();
 
+  const bithDateTime = dayjs('1990-02-05T09:24:00');
+  // Years vary in length. Divisor for percentage of current year.
+  const msCurrentYear = dayjs().endOf('year') - dayjs().startOf('year');
+  const birthYear = bithDateTime.year();
+
   const tick = () => {
-    const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
-    setAge(((Date.now() - birthTime) / divisor).toFixed(11));
+    const currentYear = dayjs().year();
+    const msSoFar = dayjs() - bithDateTime.year(currentYear);
+    const currentAge = (currentYear - birthYear) + msSoFar / msCurrentYear;
+    setAge(currentAge.toFixed(11));
   };
 
   useEffect(() => {
