7732ce0db0ad374426b722789ff23f4911ef5204
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2021-05-12 14:42:39 +0530

    check

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,29 +5,27 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Dhruv Doshi's personal website. Dalhousie University Studnet, "
-    + 'founder of DCS-BBN, and GTU Alumni.'}
+    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
+    + 'co-founder and CTO of Arthena, and YC Alumni.'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">What are you looking for?</Link></h2>
-          <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-            or you can check out my {' '}
-            <Link to="/resume">resume</Link>, {' '}
-            <Link to="/projects">projects</Link>, {' '}
-            view <Link to="/stats">site statistics</Link>, {' '}
-            or <Link to="/contact">contact</Link> me.
+          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <p>
+            A beautiful, responsive, statically-generated,
+            react application written with modern Javascript.
           </p>
-          <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
         </div>
       </header>
-      <div className="title">
-        <h2>Latest Updates!</h2>
-      </div>
-      <p>
-        Check out the blog page for new blogs {' '} <Link to="/resume"> here</Link> .
+      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+        or you can check out my {' '}
+        <Link to="/resume">resume</Link>, {' '}
+        <Link to="/projects">projects</Link>, {' '}
+        view <Link to="/stats">site statistics</Link>, {' '}
+        or <Link to="/contact">contact</Link> me.
       </p>
+      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
     </article>
   </Main>
 );
