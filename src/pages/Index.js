5608fee88e1df5c5fdce9c790675bb8cba798b7b
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2021-05-12 14:04:08 +0530

    minor update in the page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,24 +10,24 @@ const Index = () => (
     + 'founder of DCS-BBN, and GTU Alumni.'}
   >
     <article className="post" id="index">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            A responsive, statically-generated, react application written with
-            modern Javascript and hosted on Github Pages.
-          </p>
-          <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-            or you can check out my {' '}
-            <Link to="/resume">resume</Link>, {' '}
-            <Link to="/projects">projects</Link>, {' '}
-            view <Link to="/stats">site statistics</Link>, {' '}
-            or <Link to="/contact">contact</Link> me.
-          </p>
-          <p> Source available <a href="https://github.com/dhruvdoshi/dhruvdoshi.github.io">here</a>.</p>
-          <ContactIcons />
-        </div>
-      </header>
+      {/* <header> */}
+      <div className="title">
+        <h2 data-testid="heading"><Link to="/">What to look for ?</Link></h2>
+        {/* <p>
+          A responsive, statically-generated, react application written with
+          modern Javascript and hosted on Github Pages.
+        </p> */}
+        <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+          or you can check out my {' '}
+          <Link to="/resume">resume</Link>, {' '}
+          <Link to="/projects">projects</Link>, {' '}
+          view <Link to="/stats">site statistics</Link>, {' '}
+          or <Link to="/contact">contact</Link> me.
+        </p>
+        <p> Source available <a href="https://github.com/dhruvdoshi/dhruvdoshi.github.io">here</a>.</p>
+        <ContactIcons />
+      </div>
+      {/* </header> */}
     </article>
   </Main>
 );
