d8451ea5c54b3f3fc3ec82b7bb95a7ff0533ae2b
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2021-05-12 15:18:26 +0530

    check

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,11 +11,15 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+          <h2 data-testid="heading"><Link to="/">What are you looking for?</Link></h2>
+          <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+            or you can check out my {' '}
+            <Link to="/resume">resume</Link>, {' '}
+            <Link to="/projects">projects</Link>, {' '}
+            view <Link to="/stats">site statistics</Link>, {' '}
+            or <Link to="/contact">contact</Link> me.
           </p>
+          <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
