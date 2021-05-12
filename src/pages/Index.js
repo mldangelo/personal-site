f4143ffaa710ea3f357ab31f05c1f7028562f88f
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2021-05-12 15:21:28 +0530

    final front page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -19,17 +19,11 @@ const Index = () => (
             view <Link to="/stats">site statistics</Link>, {' '}
             or <Link to="/contact">contact</Link> me.
           </p>
-          <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+          <p> Source available <a href="https://github.com/dhruvdoshi/dhruvdoshi.github.io/">here</a>.</p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
-      </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <h2>Latest Updates!!</h2>
+      <p> Please find the lastest blogs <Link to="/resume">here</Link> .</p>
     </article>
   </Main>
 );
