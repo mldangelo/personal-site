7a6e4aba69eaa31f5a4b5888f32236e677bc478a
Dhruv Doshi <160770107043@socet.edu.in>
2021-05-14 07:32:46 +0530

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,7 +5,7 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Dhruv Doshi personal website.'}
+    description={"Dhruv Doshi's personal website."}
   >
     <article className="post" id="index">
       <header>
