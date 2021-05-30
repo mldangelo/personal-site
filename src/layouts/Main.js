d0ab16d2be74174bd0ada676a2d9bc7a997a899b
Noah Trueblood <40576412+notrueblood@users.noreply.github.com>
2021-05-30 09:24:06 -0400

    Fix title change (#288)
    * Fix title change
    
    Co-authored-by: Noah Trueblood <notrueblood>

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo">
+    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo" defer={false}>
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
