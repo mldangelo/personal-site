2cebe0a7b8f714cf3ed16a9656b5dad99c67df05
Dhruv Doshi <160770107043@socet.edu.in>
2021-08-07 17:42:54 -0300

    Update Blogs.js

--- src/data/Blogs.js
+++ src/data/Blogs.js
@@ -1,6 +1,14 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'First Day in Canada!',
+    subtitle: '',
+    image: '/images/blogs/IMG-20210727-WA0002.jpg',
+    date: '2021-07-12',
+    desc:
+      'Today, I amd my friend landed in Canada, it is our first day out of India! ',
+  },
+  {
     title: 'A day ended with walk!',
     subtitle: '',
     image: '/images/blogs/12-05-21.jpg',
@@ -9,14 +17,6 @@ const data = [
       'Today, I worked on the personal portfolio website and sucessfully hosted '
       + 'it on github pages after 4 days of work! At the end I went outdoor for a walk '
       + 'and that dissolved the stress out of my life!',
-  },
-  {
-    title: 'First Day in Canada!',
-    subtitle: '',
-    image: '/images/blogs/IMG-20210727-WA0002.jpg',
-    date: '2021-07-12',
-    desc:
-      'Today, I amd my friend landed in Canada, it is our first day out of India! ',
   },
 ];
 
