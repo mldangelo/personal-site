84b4d323ff42ab2a8112d652eb8d6c5e2071f953
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2022-01-12 23:44:52 -0400

    rollabck

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -1,35 +1,11 @@
 const positions = [
   {
-    company: 'Dalhousie University',
-    position: 'Teaching Assistant',
-    link: 'https://www.dal.ca',
-    daterange: 'September 2021 - Present',
-    points: [
-      'CSCI - 5409 - Advanced Cloud Computing',
-      'CSCI - 2134 - Software Development',
-      'CSCI - 1109 - Introduction to Data Science',
-      'CSCI - 1110/1105 - Introduction to computer Science',
-      'Solved Student Queries and took Lab sessoins with more than 60 students in each lab',
-    ],
-  },
-  {
-    company: 'Dalhousie University',
-    position: 'Research Assistant',
-    link: 'https://www.dal.ca',
-    daterange: 'September 2021 - December 2021',
-    points: [
-      'Worked on Project which develops tracing tables for given system of code',
-      'Added dunctionality to graphically enable to flow of an algorithm',
-      'Developed parser to take in draw.io images and get the relevant tracing table'
-    ],
-  },
-  {
     company: 'DCS_BBN',
     position: 'Founder',
     link: 'https://dhruv25071999.wixsite.com/dcs-bbn',
     daterange: 'January 2018 - January 2020',
     points: [
-      'Developed Cloud System based on Blockchain network',
+      'Developed Cloud System based on Blockchain network.',
       'Build with Python, Flask, Ethereum, Erc-20, Truffle and Metamask',
       'Other tools, Docker, Kubernetes, Jira, AWS',
       'Awards: Top 10 project out of 1500 in Hackathlon SRM Chennai',
