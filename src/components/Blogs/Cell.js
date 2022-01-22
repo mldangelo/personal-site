0e8f308a162d5cac600a82d0a061e5b6bbc8e5c6
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2022-01-22 18:49:52 -0400

    adding date and floating system

--- src/components/Blogs/Cell.js
+++ src/components/Blogs/Cell.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import dayjs from 'dayjs';
+import dayjs from 'dayjs';
 
 const Cell = ({ data }) => (
   <div>
-    <h3>
-      <a href={data.link}>{data.title}</a><br />
-    </h3>
+    <h6>
+      <span style="float:left;">
+        <a href={data.link}>{data.title}</a><br />
+      </span>
+      <span style="float:right;">
+      <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
+      </span>
+    </h6>
   </div>
 );
 
@@ -15,7 +20,7 @@ Cell.propTypes = {
     title: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
     // image: PropTypes.string.isRequired,
-    // date: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
     // desc: PropTypes.string.isRequired,
   }).isRequired,
 };
--- src/data/Blogs.js
+++ src/data/Blogs.js
@@ -3,14 +3,18 @@ const data = [
   {
     title: 'What is Cloud Computing?',
     link: 'https://dhruvdoshi.github.io/blog/2021/02/25/what-is-cloud-computing',
+    date: '2020-01-23',
   },
   {
     title: 'What is cryptocurrency?',
     link: 'https://dhruvdoshi.github.io/blog/2021/01/31/what-is-cryptocurrency',
+    date: '2020-01-22',
+
   },
   {
     title: 'How Banks could retaliate to the blockchain industry',
     link: 'https://dhruvdoshi.github.io/blog/2020/12/31/how-banks-could-retaliate',
+    date: '2020-01-21',
   },
 ];
 
