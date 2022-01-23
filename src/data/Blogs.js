8ad644fd6f42a0055fece5e7677469efab419baf
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2022-01-22 20:29:32 -0400

    added all the relevant post

--- src/data/Blogs.js
+++ src/data/Blogs.js
@@ -3,18 +3,126 @@ const data = [
   {
     title: 'What is Cloud Computing?',
     link: 'https://dhruvdoshi.github.io/blog/2021/02/25/what-is-cloud-computing',
-    date: '2020-01-23',
   },
   {
     title: 'What is cryptocurrency?',
     link: 'https://dhruvdoshi.github.io/blog/2021/01/31/what-is-cryptocurrency',
-    date: '2020-01-22',
-
+  },
+  {
+    title: 'How Blockchain Could Disrupt Banking Industry?',
+    link: 'https://dhruvdoshi.github.io/blog/2020/12/01/how-blockchain-could-distupt-banking',
   },
   {
     title: 'How Banks could retaliate to the blockchain industry',
     link: 'https://dhruvdoshi.github.io/blog/2020/12/31/how-banks-could-retaliate',
-    date: '2020-01-21',
+  },
+  {
+    title: 'What is Nodes in Blockchain?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/01/what-is-nodes-in-blockchain',
+  },
+  {
+    title: 'Time Stamping in Blockchain and Cryptocurrencies?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/02/what-is-timestamping-in-blockchain',
+  },
+  {
+    title: 'Where could be Blockchain be used?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/10/01/where-could-be-blockchain-used',
+  },
+  {
+    title: 'What if we combine Blockchain and Cloud Computing?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/10/31/what-if-we-combine-blockchain-and-cloud',
+  },
+  {
+    title: 'Downtime with Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/16/downtime-in-cloud-computing',
+  },
+  { 
+    title: 'What is the weakness of Blockchain?? (Everything is not perfect!!)',
+    link: 'https://dhruvdoshi.github.io/blog/2019/12/01/what-are-weaknes-of-blockchain',
+  },
+  {
+    title: 'What is Mining of Block in Blockchain and Cryptocurrency?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/03/what-is-mining-in-blockchain',
+  },
+  {
+    title: 'What are Crypto wallets in Blockchan and Cryptocurrencies ?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/04/what-is-wallet-in-blockchain',
+  },
+  {
+    title: 'Why blockchain is portrayed as anonymous?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/05/what-is-anonimity-in-blockchain',
+  },
+  {
+    title: 'What is Transaction Fees in Blockchain?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/06/what-is-transaction-fees-in-blockchain',
+  },
+  {
+    title: 'What are Crypto Exchanges?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/07/what-is-exchanges-in-blockchain',
+  },
+  {
+    title: 'Automatic Swaps in Blockchain',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/08/what-is-automativ-swaps-in-blockchain',
+  },
+  {
+    title: 'What are ATMs in Cryptocurrency and Blockchain?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/09/what-is-cryptocurrency-atm',
+  },
+  {
+    title: 'What is the initial coin offering in Blockchain?',
+    link: 'https://dhruvdoshi.github.io/blog/2019/09/10/what-is-cryptocurrency-ico',
+  },
+  {
+    title: 'Infrastructure as a Service(IaaS) - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/01/infrastructure-as-a-service',
+  },
+  {
+    title: 'Software as a Service(SaaS) - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/02/software-as-a-service',
+  },
+  {
+    title: 'Platform as a Service(PaaS) - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/03/platform-as-a-service',
+  },
+  {
+    title: 'Mobile Backend as a Service(MBaaS) - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/04/mobile-backend-as-a-service',
+  },
+  {
+    title: 'Serverless Computing or Function as a Service(FaaS) - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/05/serverless-computing-as-a-service',
+  },
+  {
+    title: 'Public Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/06/public-cloud-in-cloud-computing',
+  },
+  {
+    title: 'Private Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/07/private-cloud-in-cloud-computing',
+  },
+  {
+    title: 'Hybrid Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/08/hybrid-cloud-in-cloud-computing',
+  },
+  {
+    title: 'Community Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/09/community-cloud-in-cloud-computing',
+  },
+  {
+    title: 'Distributed Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/10/distributed-cloud-in-cloud-computing',
+  },
+  {
+    title: 'Multi Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/11/multi-cloud-in-cloud-computing',
+  },
+  {
+    title: 'Poly Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/12/poly-cloud-in-cloud-computing',
+  },
+  {
+    title: 'HPC Cloud - Cloud Computing',
+    link: 'https://dhruvdoshi.github.io/blog/2021/02/14/hpc-cloud-in-cloud-computing',
   },
 ];
 
