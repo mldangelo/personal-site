65e94e61a1c56543887aab05081432c57aefe1d7
al-sadi <alsadi@cmu.edu>
2022-06-08 14:21:04 +0300

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,27 +5,24 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"MOHAMMED AL-SADI's personal website.'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            This site is still under construction!
           </p>
         </div>
       </header>
-      <p> Hello & Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
     </article>
   </Main>
 );
