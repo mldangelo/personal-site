b4d43a759d4ce57d78b0a3af9c077bba3e1df7e7
Mohammed Al-Sadi - iMac <alsadi@cmu.edu>
2022-06-08 15:40:56 +0300

    Update SideBar.js

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -34,8 +34,8 @@ const SideBar = () => (
     <section id="footer">
       <ContactIcons />
       <p className="copyright">&copy; MOHAMMED AL-SADI <Link to="/">al-sadi.com</Link>.</p>
-      <p className="copyright">Last updated June 8th, 2022</p>
-      <p className="copyright">Template by <a href="https://github.com/mldangelo/personal-site">Michael</a></p>
+      <p className="copyright">Last updated <span style="color:darkred">June 8th, 2022</span></p>
+      <p className="copyright"><a href="https://github.com/mldangelo/personal-site">Template</a></p>
     </section>
   </section>
 );
