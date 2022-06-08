cb831d8d3aa5ef977592a3d4fb49fa8f6eb76d11
Mohammed Al-Sadi - iMac <alsadi@cmu.edu>
2022-06-08 15:41:46 +0300

    Update SideBar.js

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -26,7 +26,7 @@ const SideBar = () => (
       </p>
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+          { /* {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>} */}
         </li>
       </ul>
     </section>
