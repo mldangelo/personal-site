91d64b0c45512c0d40365e598378db9128120dfe
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-09-08 23:20:58 +0530

    image

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.jpeg`} alt="" />
       </Link>
       <header>
         <h2>Himanshu Arora</h2>
