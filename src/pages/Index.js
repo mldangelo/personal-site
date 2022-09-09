85247841869c24ef8d611c762a9596857dc01968
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-09-09 14:01:08 +0530

    home about updated

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,11 +11,11 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">Welcome to my website</Link></h2>
           <p>A SHOWCASE OF ALL OF MY WORK, TO SHOW WHAT I CAN BRING TO THE TABLE!</p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
