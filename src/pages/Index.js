2d7d205d5062d2e05a96a8d0d96d84bec191f48d
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-09-09 13:35:47 +0530

    home about

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,20 +12,15 @@ const Index = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
+          <p>A SHOWCASE OF ALL OF MY WORK, TO SHOW WHAT I CAN BRING TO THE TABLE!</p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
     </article>
   </Main>
 );
