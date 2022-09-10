22b4a5880d4c209ca4cd36642c315d21d29bb269
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-09-10 15:04:43 +0530

    project 4/5

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ const data = [
     subtitle: "NextJS | NodeJS | MongoDB | HighchartsJS",
     link: "https://www.lacriptoteca.app/",
     image: "/images/projects/lacriptoteca.png",
-    desc: "It provides the data, knowledge, and confidence to make better investment and trading decision in Bitcoin, Ethereum and cryptocurrency markets.",
+    desc: "La Criptoteca provides the data, knowledge, and confidence to make better investment and trading decision in Bitcoin, Ethereum and cryptocurrency markets.",
   },
   {
     title: "Unqueue",
@@ -20,6 +20,20 @@ const data = [
     link: "https://unqueue.blog/",
     image: "/images/projects/unqueue-blog.png",
     desc: "The brains behind Unqueue for Work pen down their journey of launching Corporate Plans.",
+  },
+  {
+    title: "Insights Feed",
+    subtitle: "Angular | NojeJS | MySQL",
+    link: "https://insightsfeed.app/",
+    image: "/images/projects/insights.png",
+    desc: "Insights is a library of micro-survey tools to help youth leaders measure the effectiveness of their youth ministry and the spiritual health of their youth.",
+  },
+  {
+    title: "Purple Panda",
+    subtitle: "ReactJS | NojeJS | MySQL",
+    link: "https://app.purplepanda.life/",
+    image: "/images/projects/purplepanda.png",
+    desc: "Our central place for managing your spending, tracking your investments, sorting your tax and smashing goals. Here is where you learn, grow, and connect.",
   },
 ];
 
