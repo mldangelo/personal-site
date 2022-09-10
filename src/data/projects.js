a05f6dd73f5b403074ca858f06b16a7a50fec424
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-09-10 13:37:36 +0530

    project 2

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,6 +7,13 @@ const data = [
     image: "/images/projects/lacriptoteca.png",
     desc: "It provides the data, knowledge, and confidence to make better investment and trading decision in Bitcoin, Ethereum and cryptocurrency markets.",
   },
+  {
+    title: "Unqueue",
+    subtitle: "GatsbyJS | NodeJS | Firebase",
+    link: "https://invoice.unqueue.app/",
+    image: "/images/projects/unqueue.png",
+    desc: "Unqueue brings the convenience of curbside, in-store, and delivery orders into one platform so you spend a lot less time waiting in line.",
+  },
 ];
 
 export default data;
