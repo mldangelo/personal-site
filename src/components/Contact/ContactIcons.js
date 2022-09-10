c04334a13f75e74b82ce2a239263d1be80795b4e
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-09-10 15:33:27 +0530

    target

--- src/components/Contact/ContactIcons.js
+++ src/components/Contact/ContactIcons.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import data from '../../data/contact';
+import data from "../../data/contact";
 
 const ContactIcons = () => (
   <ul className="icons">
     {data.map((s) => (
       <li key={s.label}>
-        <a href={s.link}>
+        <a href={s.link} target="_blank">
           <FontAwesomeIcon icon={s.icon} />
         </a>
       </li>
