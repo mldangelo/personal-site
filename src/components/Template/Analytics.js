fe00d6d3c742ba2c0a61d4a0cdacabcefec13842
Alec Miller <alec@overviewanalytics.ca>
2022-11-19 16:35:32 -0500

    Update to GA4 Property * replace `react-ga` with `react-ga4` package * update GA property env var (`TRACKING` -> `MEASUREMENT`) * update GA pageview send process

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
+const { NODE_ENV, REACT_APP_GA_MEASUREMENT_ID } = process.env;
 
 if (NODE_ENV === 'production') {
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+  ReactGA.initialize(REACT_APP_GA_MEASUREMENT_ID);
 }
 
 const Analytics = () => {
@@ -13,12 +13,9 @@ const Analytics = () => {
 
   useEffect(() => {
     if (NODE_ENV === 'production') {
-      ReactGA.set({
-        page: pathname,
-      });
-      ReactGA.pageview(pathname);
+      ReactGA.send({ hitType: 'pageview', page: pathname });
     }
-  }, [pathname]);
+  });
 
   return null;
 };
