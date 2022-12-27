9f7720b6a49dffe4cd4fe0152e25ace493e99d8b
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2022-12-27 13:08:39 +0000

    Added multiple new Photographs to the website

--- src/data/pictures.js
+++ src/data/pictures.js
@@ -2,12 +2,67 @@
 const data = [
   {
     title: 'Peggys Cove ',
-    subtitle: 'DCS-BBN is a Decentralized Cloud System based on Blockchian Networking. This is Python developed code for the system.',
     image: '/images/pictures/dhruv_peggys_cove.jpg',
     date: '2022-11-11',
     link: 'https://goo.gl/maps/NZzPEVoLRmKER4jF7',
     desc:
       'One day trip to the lighthouse near Halifax, Most photographed lighthouse in Canada.',
+  },
+  {
+    title: 'Albro Lake - Halifax',
+    image: '/images/pictures/dhruv_albro.jpeg',
+    date: '2022-09-25',
+    link: 'https://goo.gl/maps/9FUzvHnyG6v4z6Uw9',
+    desc:
+      'Unplanned visit to nearby Lake!',
+  },
+  {
+    title: 'Dalhousie University - Convocation ',
+    image: '/images/pictures/dhruv_convocation.jpeg',
+    date: '2022-11-02',
+    link: 'https://www.dal.ca/',
+    desc:
+      'Graduation Day - Masters of Applied Computer Science! ',
+  },
+  {
+    title: 'McNab Islands',
+    image: '/images/pictures/dhruv_mcnab_1.jpeg',
+    date: '2022-10-16',
+    link: 'https://goo.gl/maps/op4H1Xh151m5G7Zd9',
+    desc:
+      'Picture after 16 Km of Trek in isolated McNab Islands',
+  },
+  {
+    title: 'Cabbot Trail - Cape Breton Islands',
+    image: '/images/pictures/dhruv_capebreton_1.jpeg',
+    date: '2022-08-01',
+    link: 'https://goo.gl/maps/mkBs6PgSeF5DJCxk9',
+    desc:
+      'Most scenic park of Nova Scotia.',
+  },
+  {
+    title: 'Streetside Image in Halifax',
+    image: '/images/pictures/halifax_random_1.jpeg',
+    date: '2022-10-15',
+    link: 'https://goo.gl/maps/T9BPQYvH9VeSsajA8',
+    desc:
+      'A random shot with roommates on streets of Halifax',
+  },
+  {
+    title: 'Putting Edge ',
+    image: '/images/pictures/halifax_last_day.jpeg',
+    date: '2022-12-02',
+    link: 'https://www.puttingedge.com/locations/halifax/',
+    desc:
+      'The last place we visited in halifax before moving to Toronto',
+  },
+  {
+    title: 'Holi Celebration - Dalhousie University',
+    image: '/images/pictures/holi_dalhousie.jpeg',
+    date: '2022-03-20',
+    link: 'https://www.dal.ca/',
+    desc:
+      'Celebration of festival of colors with Indian society of Dalhousie University',
   },
 ];
 
