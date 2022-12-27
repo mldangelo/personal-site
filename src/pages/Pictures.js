4c1580cccb810f0d895d6ce06438139458eb43e9
Dhruv Doshi <dhruvdoshi25071999@gmail.com>
2022-12-27 13:08:25 +0000

    Refactoring the code - Reformatting

--- src/pages/Pictures.js
+++ src/pages/Pictures.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import Main from '../layouts/Main';
-
 import Cell from '../components/Pictures/Cell';
 import data from '../data/pictures';
+// import { Link } from 'react-router-dom';
 
 const Pictures = () => (
   <Main
     title="Pictures"
-    description="Learn about Dhruv Doshi's projects."
+    description="See more of Dhruv Doshi's Pictures."
   >
     <article className="post" id="projects">
-      <header>
+      {/* <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/pictures">Pictures</Link></h2>
           <p>A selection of pictures that I&apos;m not too ashamed of</p>
         </div>
-      </header>
+      </header> */}
       {data.map((pictures) => (
         <Cell
           data={pictures}
