7e02cfe50e6cdc9871b64bf74d6f5a2081f2503f
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-03-07 00:32:56 -0500

    remove unused package

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,4 @@ module.exports = {
     '@babel/preset-env',
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
-
 };
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 const config = {
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
-    // map markdown
     '^.+\\.md$': 'markdown-to-jsx',
   },
 };
