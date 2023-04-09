0137b37eac7f53df9c4c1d3804737f9dca65bd82
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2023-04-09 21:15:36 +0530

    2023 update

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {},
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "react-hooks"],
+  ignorePatterns: ["node_modules/"],
+  rules: {},
+  settings: {
+    react: {
+      version: "latest", // "detect" automatically picks the version you have installed.
+    },
+  },
+};
