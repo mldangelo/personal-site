6dadd20b86e91a9e6acb21084dd7123ca05ecc18
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-04-10 18:52:52 -0400

    update resume schema

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -1,132 +1,137 @@
-const positions = [
+//
+
+/**
+ * @typedef {Object} Position
+ * Conforms to https://jsonresume.org/schema/
+ *
+ * @property {string} name - Name of the company
+ * @property {string} position - Position title
+ * @property {string} url - Company website
+ * @property {string} startDate - Start date of the position in YYYY-MM-DD format
+ * @property {string|undefined} endDate - End date of the position in YYYY-MM-DD format.
+ * If undefined, the position is still active.
+ * @property {string|undefined} summary - html/markdown summary of the position
+ * @property {string[]} highlights - plain text highlights of the position (bulleted list)
+ */
+const work = [
   {
-    company: 'Arthena',
+    name: 'Arthena',
     position: 'Co-founder, C.T.O.',
-    link: 'https://arthena.com',
-    daterange: 'January 2014 - Present',
-    positions: [{
-      title: 'Co-founder, C.T.O.',
-      daterange: 'January 2014 - Present',
-    }],
-    points: [
+    url: 'https://arthena.com',
+    startDate: '2014-01-01',
+    endDate: '2022-01-01',
+    summary: `YC W17. Series A Company funded by <a href="https://www.anthemis.com/">Athemis</a> and 
+    acquired by <a href="https://www.masterworks.com/">Masterworks</a> in 2023. 
+    `,
+    highlights: [
       'Deployed quantitative strategies to predict the value of fine art in various pricing contexts.',
       'Built production, on-line, end-to-end optimized machine learning pipelines with Pandas, Numpy, Scikit, Tensorflow, Dagster, Postgres, etc. on GCP with Kubernetes.',
       'Designed micro-service architecture around: data collection, data integrity, feature engineering, research, strategy, backtesting, deployment, and reporting.',
-      'YC Alumn. Worked on everything. Built a 16-person team. Operated company for 6+ years.',
+      'YC Alumni. Worked on everything. Built a 16-person team. Operated company for 7+ years.',
     ],
   },
   {
-    company: 'Skeptical Investments',
-    link: 'http://skepticalinvestments.biz',
-    positions: [{
-      title: 'Co-founder',
-      daterange: 'April 2017 - Present',
-    }],
-    points: [
+    name: 'Skeptical Investments',
+    position: 'Co-founder',
+    url: 'http://skepticalinvestments.biz',
+    startDate: '2017-04-01',
+    highlights: [
       'Founded a micro-vc fund to throw token amounts of money at problems I find interesting.',
       'Created the InstaSafe. Fast, automated investment docs for YC companies.',
     ],
   },
   {
-    company: 'Enveritas',
-    link: 'http://enveritas.org',
-    positions: [{
-      title: 'ML Engineering Contractor',
-      daterange: 'September 2016 - 2018',
-    }],
-    points: [
+    name: 'Enveritas',
+    position: 'ML Engineering Contractor',
+    url: 'http://enveritas.org',
+    startDate: '2016-09-01',
+    endDate: '2018-01-01',
+    highlights: [
       'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
       'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
-      'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM’s, and other techniques.',
+      'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM\'s, and other techniques.',
     ],
   },
   {
-    company: 'Zenysis',
-    link: 'https://zenysis.com',
-    positions: [{
-      title: 'Software Engineering Contractor',
-      daterange: 'February 2016 - March 2016',
-    }],
-    points: [
+    name: 'Zenysis',
+    position: 'Software Engineering Contractor',
+    url: 'https://zenysis.com',
+    startDate: '2016-02-01',
+    endDate: '2016-03-01',
+    highlights: [
       'Worked in Addis Ababa for the Ethiopian Ministry of Health and built data visualization tools in React and Flask.',
       'Ingested multiple databases with different alphabets, calendars, and without official spellings of geographic locations.',
     ],
   },
   {
-    company: 'Matroid',
-    link: 'https://matroid.com',
-    positions: [{
-      title: 'Co-founder',
-      daterange: 'July 2015 - January 2016',
-    }],
-    points: [
+    name: 'Matroid',
+    position: 'Co-founder',
+    url: 'https://matroid.com',
+    startDate: '2015-07-01',
+    endDate: '2016-01-01',
+    highlights: [
       'Developed end to end machine learning pipeline to train visual classifiers from keywords using Node.JS, Express, Keystone, MongoDB, AWS, S3, Caffe, and other technologies.',
       'Received Series A term sheets for 20M+ valuations.',
     ],
   },
   {
-    company: 'Planet',
-    link: 'https://planet.com',
-    positions: [{
-      title: 'Missions Intern',
-      daterange: 'June 2014 - January 2015',
-    }],
-    points: [
+    name: 'Planet',
+    position: 'Missions Intern',
+    url: 'https://planet.com',
+    startDate: '2014-06-01',
+    endDate: '2015-01-01',
+    highlights: [
       'Built models to improve image quality, signal to noise ratio, and dynamic range.',
       'Performed statistical analysis of image quality in Matlab and Python.  Developed flight software in C++.',
       'Organized first hackathon, prototyped hardware, and designed and built photography equipment for rocket launches.',
     ],
   },
   {
-    company: 'Planetary Resources',
-    link: 'http://planetaryresources.com',
-    positions: [{
-      title: 'Avionics Intern',
-      daterange: 'January 2014 - May 2014',
-    }],
-    points: [
+    name: 'Planetary Resources',
+    position: 'Avionics Intern',
+    startDate: '2014-01-01',
+    endDate: '2014-05-01',
+    url: 'http://planetaryresources.com',
+    highlights: [
       'Developed simulations in Matlab for Attitude Determination and Control Subsystem.',
       'Developed processes for in lab testing and characterization of various subsystems.',
       'Assembled flight hardware in cleanroom.',
     ],
   },
   {
-    company: 'Facebook',
-    link: 'https://facebook.com',
-    positions: [{
-      title: 'Software Engineer Intern',
-      daterange: 'June 2013 - September 2013',
-    }],
-    points: [
+    name: 'Facebook',
+    position: 'Software Engineer Intern',
+    url: 'https://facebook.com',
+    startDate: '2013-06-01',
+    endDate: '2013-09-01',
+    highlights: [
       'Developed software in python for automated testing of servers.',
       'Performed statistical analysis with R, HIVE to assist in triage of malfunctioning servers.',
-      'Worked with vendors and ODM’s during triage to assist in risk mitigation.',
+      'Worked with vendors and ODM\'s during triage to assist in risk mitigation.',
     ],
   },
   {
-    company: 'SEDS-USA',
-    link: 'http://seds.org',
-    positions: [{
-      title: 'At Large Board Member',
-      daterange: 'October 2013 - October 2014',
-    }],
-    points: [
+    name: 'SEDS-USA',
+    position: 'At Large Board Member',
+    url: 'http://seds.org',
+    startDate: '2013-10-01',
+    endDate: '2014-10-01',
+    highlights: [
       'Elected to Board of Directors of the USA\'s largest student space advocacy group based on 5+ years of work with SEDS.',
       'Responsibilities included: organizational strategy, conference presentations, fundraising, special projects, promoting SEDS nationally and internationally, and photographing conferences.',
     ],
   },
   {
-    company: 'UB Nanosatellite Program',
-    link: 'https://ubnl.space/',
-    positions: [{
-      title: 'Co-founder, Program Manager',
-      daterange: 'October 2010 - June 2012',
-    }],
-    points: [
+    name: 'UB Nanosatellite Program',
+    position: 'Co-founder, Program Manager',
+    url: 'https://ubnl.space/',
+    startDate: '2010-10-01',
+    endDate: '2012-06-01',
+    highlights: [
       'Coauthored grant to build a multi-spectral imaging satellite as part of the AFRL University Nanosatellite Program.',
       'Lead a team of 60 students through satellite development life cycle. Served as a technical expert, acquired intimate working knowledge of satellite subsystems.  Solicited funding through NASA, AFOSR, and several corporate sponsors.',
     ],
   },
 ];
 
-export default positions;
+export default work;
