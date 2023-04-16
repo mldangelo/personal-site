315b2462cc072ee44091131f02d1654ae9ba5601
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-04-16 18:24:27 -0400

    add summary sections

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -17,17 +17,18 @@ const work = [
     position: 'VP Engineering & Head of AI',
     url: 'https://smileidentity.com',
     startDate: '2022-01-01',
-    summary: `Smile integrates with government ID systems to verify identities. We build ML powered APIs
-    to compare user provided selfies to ID documents. I was promoted from Director of Engineering to VP
-    Engineering in April 2022, and then to Head of AI in November 2022. I am a hands-on technical leader.
-    I lead a 20+ person product-driven engineering org. I directly manage ~8 engineers and spend >50% of
-    my time coding.`,
+    summary: `Smile builds ML powered APIs to compare user provided selfies to ID documents. I provide
+    technical leadership on high-impact projects, influence and coach a distributed team of engineers,
+    and facilitate alignment and clarity across teams on goals, outcomes, and timelines. I was promoted
+    from Director of Engineering to VP of Engineering in April 2022, and then to VP of Engineering and Head
+    of AI in November 2022. I lead a 20+ person engineering org. I directly manage ~8 engineers and spend
+    >50% of my time writing code.`,
     highlights: [
-      'Established good engineering practices, including: re-factoring stand-ups and engineering meetings, bug tracking, improved culture for code reviews, blameless post-mortems, and retrospectives.',
+      'Redesigned engineering processes for stand-ups and engineering meetings, bug tracking, improved culture for code reviews, blameless post-mortems, and retrospectives.',
       'Re-architected engineering hiring and onboarding processes. Recruited several strong engineers.',
       'Reorged engineering teams to focus on product delivery. Created a new team to focus on ML infrastructure.',
       'Lead re-design of internal APIs for inference. Built new computer vision pipelines for NIST/iBeta liveness certifications.',
-      'Pitched -> launched a fraud detection product based on 1-N facial recognition using embeddings and vector search.',
+      'Pitched, designed, developed, deployed, and maintain a fraud detection product based on 1-N facial recognition using embeddings and vector search.',
     ],
   },
   {
@@ -35,8 +36,8 @@ const work = [
     position: 'Co-founder',
     url: 'http://skepticalinvestments.biz',
     startDate: '2017-04-01',
-    summary: `Skeptical Investments is a micro-vc fund. We invest in people and and problems we find interesting.
-    We are primarily focused on Angel, Seed, and Series-A YC companies.`,
+    summary: `Skeptical Investments is a micro-vc fund focused on Angel, Seed, and Series-A
+    YC companies.`,
     highlights: [
       'Created the InstaSafe. Fast, automated investment docs for YC companies.',
       'Advise several startups in the fund.',
@@ -44,19 +45,21 @@ const work = [
   },
   {
     name: 'Arthena',
-    position: 'Co-founder, C.T.O.',
+    position: 'Co-founder & C.T.O.',
     url: 'https://arthena.com',
     startDate: '2014-01-01',
     endDate: '2022-01-01',
-    summary: `Arthena is a post-series A Company funded by <a href="https://www.anthemis.com/">Athemis</a>, 
-    <a href="https://foundationcapital.com/>Foundation Capital</a>, and <a href="https://ycombinator.com/">YC</a>.
-    We were acquired by <a href="https://www.masterworks.com/">Masterworks</a> in 2023. I ran the product and
-    engineering orgs for 8 years.`,
+    summary: `Arthena is a Series A Company funded by <a href="https://www.anthemis.com/">Athemis</a>,
+    <a href="https://foundationcapital.com">Foundation Capital</a>, and <a href="https://ycombinator.com/">YC</a>.
+    We develop quantitative strategies to predict the value of fine art and build investment products.
+    I ran a 20-person product and engineering org for 8 years. We were acquired by
+    <a href="https://www.masterworks.com/">Masterworks</a> in 2023.`,
     highlights: [
-      'Deployed quantitative strategies to predict the value of fine art in various pricing contexts.',
-      'Built production, on-line, end-to-end optimized machine learning pipelines with Pandas, Numpy, Scikit, Tensorflow, Dagster, Postgres, etc. on GCP with Kubernetes.',
+      'Built production, online, end-to-end optimized machine learning pipelines for time-series prediction.',
       'Designed micro-service architecture around: data collection, data integrity, feature engineering, research, strategy, backtesting, deployment, and reporting.',
-      'YC Alumni. Worked on everything. Built a 20-person team. Operated company for 8+ years.',
+      "Set and communicated team priorities that supported the broader organization's goals. Aligned strategy, processes, and decision-making across teams.",
+      "Set clear expectations with individuals based on their level and role and aligned them to the broader organization's goals. Met regularly with individuals to discuss performance and development, and provided feedback and coaching.",
+      'Developed the long-term technical vision and roadmap within, and often beyond, the scope of my teams. Evolved the roadmap to meet anticipated future requirements and infrastructure needs.',
     ],
   },
   {
@@ -65,6 +68,9 @@ const work = [
     url: 'http://enveritas.org',
     startDate: '2016-09-01',
     endDate: '2018-01-01',
+    summary: `Enveritas is a non-profit that works with coffee farmers to improve their livelihoods.
+    I joined on day one and helped build a variety of tools to collect and analyze data. I also advised
+    on technical strategy, helped hire engineers, and advised on their YC application.`,
     highlights: [
       'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
       'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
@@ -77,6 +83,10 @@ const work = [
     url: 'https://zenysis.com',
     startDate: '2016-02-01',
     endDate: '2016-03-01',
+    summary: `Zenysis develops data analysis and visualization tools for government agencies and international organizations.
+    Their solutions help improve decision-making by integrating and analyzing large-scale datasets from various sources.
+    I was the second contributor to the codebase. We built data visualization tools and tackled complex data ingestion
+    challenges for the Ethiopian Ministry of Health.`,
     highlights: [
       'Worked in Addis Ababa for the Ethiopian Ministry of Health and built data visualization tools in React and Flask.',
       'Ingested multiple databases with different alphabets, calendars, and without official spellings of geographic locations.',
@@ -88,9 +98,12 @@ const work = [
     url: 'https://matroid.com',
     startDate: '2015-07-01',
     endDate: '2016-01-01',
+    summary: `Matroid is a computer vision platform for creating and deploying detectors. Detectors help customers identify objects,
+    events, or patterns in images and videos without requiring machine learning or coding expertise. I played a crucial role in
+    defining the company's vision. I was responsible for architecting and building the initial product. I left shortly after the
+    Series A to focus on Arthena.`,
     highlights: [
-      'Developed end to end machine learning pipeline to train visual classifiers from keywords using Node.JS, Express, Keystone, MongoDB, AWS, S3, Caffe, and other technologies.',
-      'Received Series A term sheets for 20M+ valuations.',
+      'Developed end to end machine learning pipeline to train visual classifiers from keywords using Caffe, Node.JS, Redis, MongoDB, and other technologies.',
     ],
   },
   {
