7f4cf0e6a6ecfc36422606d26da1f08b3a4c38fa
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-10-30 01:48:42 -0400

    revert jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
-module.exports = {
+const config = {
   // All the playwright options can be found here: https://github.com/playwright-community/jest-playwright#options
   preset: 'jest-playwright-preset',
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'babel-jest',
+    '^.+\\.md$': 'markdown-to-jsx',
+  },
   transform: {
     '^.+\\.ts?$': 'ts-jest',
     '^.+\\.jsx?$': 'babel-jest',
   },
 };
+
+module.exports = config;
