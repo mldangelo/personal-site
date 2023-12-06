71bd7ea7291f202a12a4177b87b358af9d53654b
nmrenyi <nmrenyi@outlook.com>
2023-12-06 11:16:24 +0800

    info: update email

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -82,9 +82,9 @@ const EmailLink = ({ loopMessage }) => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+      <a href={validateText(message) ? 'mailto:renyi1006@gmail.com' : ''}>
         <span>{message}</span>
-        <span>@mldangelo.com</span>
+        <span>@gmail.com</span>
       </a>
     </div>
   );
