374047caee0924b6ef3e13604b4e65c07370c080
nmrenyi <nmrenyi@outlook.com>
2023-12-06 10:58:28 +0800

    info: adapt sidebar info

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,18 +12,17 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Aida Ren 任一</h2>
+        <p><a href="mailto:renyi1006@gmail.com">renyi1006@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Aida. I&apos;m a PhD Student in Tsinghua University.
+        Now I&apos;m somewhere deciding my future,
+        whether it&apos;s about continuing my research into recommendation system
+        or switching to a more challenging and theoretical direction, quantum computing.
       </p>
       <ul className="actions">
         <li>
@@ -34,7 +33,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Aida ren <Link to="/">www.renyi1006.com</Link>.</p>
     </section>
   </section>
 );
