3e84f434ae2e35b99b95621a531a7a64e5f2583a
nmrenyi <nmrenyi@outlook.com>
2023-12-06 11:02:43 +0800

    info: adapt front  page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,16 +5,14 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
+    description={"Aida Ren's personal website. A PhD student in Tsinghua University."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
+          <h2><Link to="/">Hi there!</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Enchanted to meet you!
           </p>
         </div>
       </header>
@@ -25,7 +23,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/nmrenyi/plain-personal-site">here</a>.</p>
     </article>
   </Main>
 );
