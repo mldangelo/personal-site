66c7d8d6ff4d10d88d6dd4795927ae1d1c4022e7
nmrenyi <nmrenyi@outlook.com>
2023-12-06 11:37:40 +0800

    info: update birth time

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -5,7 +5,7 @@ const Age = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1999-08-27T16:24:00');
+    const birthTime = new Date('1999-10-06T16:00:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
