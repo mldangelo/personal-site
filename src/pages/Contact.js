eecc8a8e00198a426e1c9d105b54b7a58e9ca71d
Kyle McMaster <67760150+KyleJMcMaster@users.noreply.github.com>
2023-12-27 21:09:15 -0500

    Update Contact.js
    Changed email to kylejmcmaster@gmail.com

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ hi@mldangelo.com"
+    description="Contact Kyle McMaster via email @ kylejmcmaster@gmail.com"
   >
     <article className="post" id="contact">
       <header>
