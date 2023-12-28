d7bb20ad2db285f7c2a38718e03038182fcfeec1
Kyle McMaster <67760150+KyleJMcMaster@users.noreply.github.com>
2023-12-27 21:33:22 -0500

    Update SideBar.js

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,18 +12,15 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Kyle McMaster</h2>
+        <p><a href="mailto:kylejmcmaster@gmail.com">kylejmcmaster@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Kyle. I am a fourth year software engineering student at <a href="https://www.mcmaster.ca/"> McMaster University </a> with a minor in math and a passion for artificial intelligence.
+  This website is my place to show some projects I'm working on.
       </p>
       <ul className="actions">
         <li>
@@ -34,7 +31,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Kyle McMaster <Link to="/">kylemcmaster.ca</Link>.</p>
     </section>
   </section>
 );
