f19732729afda8c837f97a12fd9b0257e1cd5f6e
Kyle McMaster <67760150+KyleJMcMaster@users.noreply.github.com>
2023-12-27 21:11:27 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,7 @@ const About = () => {
   return (
     <Main
       title="About"
-      description="Learn about Michael D'Angelo"
+      description="Learn about Kyle McMaster"
     >
       <article className="post markdown" id="about">
         <header>
