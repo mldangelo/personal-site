8a9c65ac3af7884f60e1532a1dfc6907e63588b6
mariusmercier <mariusmercier1@gmail.com>
2024-01-01 16:36:41 +0100

    test

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,11 +9,11 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.png`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Marius Mercier</h2>
+        <p><a href="mailto:mariusmercier1@gmail.com">mariusmercier1@gmail.com</a></p>
       </header>
     </section>
 
