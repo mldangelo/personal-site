dc35902f5f90dc6e2e34c5417837bcaaa6725aa4
mariusmercier <mariusmercier1@gmail.com>
2024-01-01 16:45:25 +0100

    try deploy

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,7 +19,7 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
+      <p>Hi, I&apos;m Marius. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
         and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
         I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
         , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
