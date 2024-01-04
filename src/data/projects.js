1bc323d15cb063800d4749c9fa7c30a0c7fddb80
marc131183 <marc.groeling@gmail.com>
2024-01-04 16:12:19 +0100

    fix typos, add projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,43 +1,64 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Master thesis: E-Waste Tracking with Blockchain',
+    subtitle: '',
+    image: '/images/projects/master_thesis_1.png',
+    date: '2023-06-01',
+    link: 'https://ntnuopen.ntnu.no/ntnu-xmlui/bitstream/handle/11250/3093301/no.ntnu%3Ainspera%3A146719271%3A95368941.pdf?sequence=1',
+    desc:
+      'Created a waste tracking system with blockchain that uses a byzantine fault-tolerant consensus algorithm. '
+      + 'A unique barcode is assigned to each electronic device and then added to the blockchain.',
+  },
+  {
+    title: 'Waste Classification System',
+    subtitle: '',
+    image: '/images/projects/waste_classification.png',
+    date: '2022-12-01',
+    link: 'https://github.com/mgroling/WasteClassification',
+    desc:
+      'Built a system that can classify waste together with another student. '
+      + 'Furthermore, a dataset of ~5000 images was collected from a local waste site. '
+      + 'On this dataset, several image classification models were trained and used to classify waste. '
+      + 'The work was presented in the Intelligent Systems Conference 2023.',
+  },
+  {
     title: 'Image Generator using Raycasts',
     subtitle: '',
     image: '/images/projects/scene_creator.png',
-    date: '2022-07-01',
+    date: '2022-08-01',
     link: 'https://github.com/mgroling/SceneCreator',
     desc:
       'Built a render engine to create high-definition images using ray-casting. '
       + 'The engine is built in C++ and covers lighting, textures, and a parallelised ray-casting procedure.',
   },
   {
-    title: 'Waste Classification System',
-    subtitle: '',
-    image: '/images/projects/waste_classification.png',
-    date: '2022-04-01',
-    link: 'https://github.com/mgroling/WasteClassification',
-    desc:
-      'Built a system that can help classifiy waste together with another student. '
-      + 'Furthermore, a dataset of ~5000 images was collected from a local waste site. '
-      + 'On this dataset, several image classification models were trained and used to classify waste. '
-      + 'The work was presented in the Intelligent Systems Conference 2023.',
-  },
-  {
     title: "Gym Rubik's Cube",
     subtitle: '',
     image: '/images/projects/rubiks_cube.png',
-    date: '2021-08-01',
+    date: '2021-10-01',
     link: 'https://github.com/mgroling/GymRubiksCube',
     desc:
       "Created a reinforcement learning environment for the 3-sided Rubik's Cube in Python. "
       + 'The environment features a 3D visualisation tool that can render any object consisting of triangles using rasterization. '
-      + 'However, it only runs on the cpu and is thus quite slow.',
+      + 'However, it only runs on the CPU and is thus quite slow.',
+  },
+  {
+    title: 'Bachelor thesis: Learning of Locomotion Data of Guppies with SQIL (RL)',
+    subtitle: '',
+    image: '/images/projects/bachelor_thesis.png',
+    date: '2021-05-01',
+    link: 'http://www.mi.fu-berlin.de/inf/groups/ag-ki/Theses/Completed-theses/Bachelor-theses/2021/Groeling/BachelorThesis-Marc-Groeling.pdf',
+    desc:
+      'Trained an artificial neural network with soft Q imitation learning (SQIL), which uses a combination of supervised learning '
+      + 'and reinforcement learning to generate training data. Hyperparameter optimisation was done and metrics to evaluate the models '
+      + 'created.',
   },
   {
     title: 'Learning of Locomotion Data of Elephant Fish',
     subtitle: '',
     image: '/images/projects/elephant_fish.jpg',
-    date: '2020-05-01',
+    date: '2020-07-01',
     link: 'https://github.com/mgroling/Elephant-Fish',
     desc:
       'Trained an LSTM neural network to predict the locomotion behaviour of elephant fish. '
--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -15,8 +15,8 @@ const work = [
   {
     name: 'Norwegian University of Science and Technology',
     position: 'Teaching Assistant',
-    summary: `I worked as a teaching assistant for a course in algortihms and data structures for the bachelor level. 
-    There, I helped students with assignments during lab sessions and also helped with grading of assignments.`,
+    summary: `I worked as a teaching assistant for a course in algorithms and data structures for the bachelor level. 
+    There, I helped students with assignments during lab sessions and also helped with grading assignments.`,
     url: 'https://www.ntnu.edu/',
     startDate: '2022-08',
     endDate: '2022-12',
@@ -25,7 +25,7 @@ const work = [
     name: 'Free University of Berlin',
     position: 'Teaching Assistant',
     summary: `Working as a teaching assistant, I contributed to the delivery of a course in database systems.
-    There, I lead two tutorials of about 25 students each on my own and prepared and went through exercises for the students in these.
+    There, I led two tutorials of about 25 students each on my own and prepared and went through exercises for the students in these.
     Furthermore, I helped with the creation and grading of assignment sheets and the exam with three other TAs for about 200 students.`,
     url: 'https://www.fu-berlin.de/en/index.html',
     startDate: '2022-08',
@@ -36,7 +36,7 @@ const work = [
     position: 'Data Science Working Student',
     summary: `Mainly worked on the maintenance and improvement of a machine learning model that predicts prices for used cars.
     Cars from the Daimler AG have highly individual configurations, which is why it is helpful to have a model that can predict
-    the price of these when they are sold again. I also took part in the development of other machine learning projects.`,
+    the price of these when they are sold again. I also took part in the development of other machine-learning projects.`,
     url: 'https://group.mercedes-benz.com/de/',
     startDate: '2019-08',
     endDate: '2019-12',
