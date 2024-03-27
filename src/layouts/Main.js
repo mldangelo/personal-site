ffc7221bb850e6bab743225b7eeec0ef0a037c6e
Ebrahim Sharifnia <ssharifn@vols.utk.edu>
2024-03-26 20:45:46 -0400

    edit titles

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo" defer={false}>
+    <Helmet titleTemplate="%s | Ebrahim Sharifnia" defaultTitle="Ebrahim Sharifnia" defer={false}>
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Michael D'Angelo's personal website.",
+  description: "Ebrahim Sharifnia's personal website.",
 };
 
 export default Main;
