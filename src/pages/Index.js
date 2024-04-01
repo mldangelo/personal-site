98a3b35942dfc0db1b58d9fb8e4b611ff6ca5b61
Ebrahim Sharifnia <ssharifn@vols.utk.edu>
2024-03-31 23:54:46 -0400

    index test

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,16 +5,14 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
+    description={"Ebrahim Sharifnia's personal website"}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
+          <h1><Link to="/">About this site</Link></h1>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Here, you can get to know me a bit!
           </p>
         </div>
       </header>
@@ -25,7 +23,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      {/* <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p> */}
     </article>
   </Main>
 );
