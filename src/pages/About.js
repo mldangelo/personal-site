cd0453b9f3426075f69a1f5deafa69330dfbf59c
Ebrahim Sharifnia <ssharifn@vols.utk.edu>
2024-04-01 23:43:11 -0400

    edit name

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,7 @@ const About = () => {
   return (
     <Main
       title="About"
-      description="Learn about Michael D'Angelo"
+      description="Learn about Ebrahim Sharifnia"
     >
       <article className="post markdown" id="about">
         <header>
