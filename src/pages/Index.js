026643445458e04f63d83601404d0fe8a77b0ad6
Ebrahim Sharifnia <ssharifn@vols.utk.edu>
2024-04-01 23:34:45 -0400

    Edit intro

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,25 +5,31 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Ebrahim Sharifnia's personal website"}
+    description={"Ebrahim Sharifnia's personal website. Explore my professional journey, projects, and ways to connect with me."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h1><Link to="/">About this site</Link></h1>
+          <h1><Link to="/">Welcome!</Link></h1>
           <p>
-            I'm Ebrahim Sharifnia, a PhD candidate in engineering with a passion for data science and optimization. With a background in Industrial Engieering and Statistics, I've contributed to various projects that you can explore some of them <Link to="/projects">here</Link>. This website serves as a media to my portfolio, including my <Link to="/resume">resume</Link> and a collection of personal and professional projects that reflect my skills and interests.
+            Discover my professional journey and projects.
           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+      <p>
+        I&rsquo;m Ebrahim a PhD candidate in engineering with a passion for data science and optimization. With a background in Industrial Engieering and Statistics, I&rsquo;ve contributed to various projects that you can explore some of them <Link to="/projects">here</Link>. This website serves as a comprehensive portfolio of my work, including my <Link to="/resume">resume</Link> and a collection of personal and professional projects that reflect my skills and interests.
       </p>
-      {/* <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p> */}
+      {/* <p>
+        {Beyond my professional interests, I&rsquo;m enthusiastic about }
+        {[mention any hobbies or personal interests]. }
+        {You can also find some insights into my website&rsquo;s performance and visitor statistics}
+        { <Link to="/stats">here</Link>.}
+      </p> */}
+      <p>
+        Feel free to <Link to="/contact">contact</Link> me for collaboration opportunities, questions, or to simply say hello. I&rsquo;m always open to discussing new projects and ideas.
+      </p>
+      {/* projects, consider uncommenting and updating the link below. */}
+      {/* <p> Source available <a href="[Your GitHub Repository Link]">here</a>.</p> */}
     </article>
   </Main>
 );
