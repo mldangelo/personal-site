12d3d280e67b3b7640ec85b8475f207bf5f6e5f4
Ebrahim Sharifnia <ssharifn@vols.utk.edu>
2024-04-01 23:45:31 -0400

    edit names

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import Main from '../layouts/Main';
+import Main from '../layouts/Main';
 
-// import Personal from '../components/Stats/Personal';
-// import Site from '../components/Stats/Site';
+import Personal from '../components/Stats/Personal';
+import Site from '../components/Stats/Site';
 
-// const Stats = () => (
-//   <Main
-//     title="Stats"
-//     description="Some statistics about Michael D'Angelo and mldangelo.com"
-//   >
-//     <article className="post" id="stats">
-//       <header>
-//         <div className="title">
-//           <h2><Link to="/stats">Stats</Link></h2>
-//         </div>
-//       </header>
-//       <Personal />
-//       <Site />
-//     </article>
-//   </Main>
-// );
+const Stats = () => (
+  <Main
+    title="Stats"
+    description="Some statistics about Ebrahim and and his impact"
+  >
+    <article className="post" id="stats">
+      <header>
+        <div className="title">
+          <h2><Link to="/stats">Stats</Link></h2>
+        </div>
+      </header>
+      <Personal />
+      <Site />
+    </article>
+  </Main>
+);
 
-// export default Stats;
+export default Stats;
