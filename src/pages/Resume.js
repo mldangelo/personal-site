1edd4c68159fa12d93f83b9a443c8ad6ef02d4ce
mariusmercier <mariusmercier1@gmail.com>
2024-05-10 18:37:04 +0200

    test

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,30 +3,32 @@ import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
-import Education from '../components/Resume/Education';
-import Experience from '../components/Resume/Experience';
+// import Education from '../components/Resume/Education';
+// import Experience from '../components/Resume/Experience';
 import Skills from '../components/Resume/Skills';
 // import Courses from '../components/Resume/Courses';
-import References from '../components/Resume/References';
+// import References from '../components/Resume/References';
 
 // import courses from '../data/resume/courses';
-import degrees from '../data/resume/degrees';
-import work from '../data/resume/work';
+// import degrees from '../data/resume/degrees';
+// import work from '../data/resume/work';
 import { skills, categories } from '../data/resume/skills';
 
 // NOTE: sections are displayed in order defined.
 const sections = {
-  Education: () => <Education data={degrees} />,
-  Experience: () => <Experience data={work} />,
+  // Education: () => <Education data={degrees} />,
+  // Experience: () => <Experience data={work} />,
   Skills: () => <Skills skills={skills} categories={categories} />,
   // Courses: () => <Courses data={courses} />,
-  References: () => <References />,
+  // References: () => <References />,
 };
+
+const { PUBLIC_URL } = process.env;
 
 const Resume = () => (
   <Main
     title="Resume"
-    description="Michael D'Angelo's Resume. Smile Identity, Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet, and Facebook."
+    description="Marius Mercier's CV"
   >
     <article className="post" id="resume">
       <header>
@@ -40,6 +42,16 @@ const Resume = () => (
           </div>
         </div>
       </header>
+      <p>
+        View my{' '}
+        <Link
+          to={`${PUBLIC_URL}/files/Marius_Mercier_cv_Apr_2024.pdf`}
+          target="_blank"
+        >
+          CV
+        </Link>
+        .
+      </p>
       {Object.entries(sections).map(([name, Section]) => (
         <Section key={name} />
       ))}
