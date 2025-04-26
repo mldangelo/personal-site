aa1f64fd4253eea0bf42420c48a877dd5f627f27
Kai Zhang <zhangkai@student.ethz.ch>
2025-04-26 23:38:54 +0200

    updated: names

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Michael D'Angelo");
+    expect(document.title).toBe("Kai Zhang");
   });
 
   it('can navigate to /about', async () => {
